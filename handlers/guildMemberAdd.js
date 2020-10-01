a5cc6f86f4c0fd5579e58e97e850ef9a34a2bd14
hollandcargovtc <71852352+hollandcargovtc@users.noreply.github.com>
2020-10-01 02:55:50 -0500

    Update guildMemberAdd.js

--- handlers/guildMemberAdd.js
+++ handlers/guildMemberAdd.js
@@ -4,22 +4,22 @@ client.on("guildMemberAdd", function(member){
     client.channels.get("758546977671086080").send({embed:{
         color: 3066993,
         title: `**Bienvenido al discord de Holland Cargo**`,
-        description: `Bienvenido ${member}`,
+        description: `Bienvenido ${member}, por favor pasala bien y diviérte`,
         thumbnail: {
-            url: member.user.avatarURL || "https://media.discordapp.net/attachments/758548412244951062/761076086385541130/H2_1_copia.png?width=943&height=475"
+            url: member.user.avatarURL || "https://media.discordapp.net/attachments/758548412244951062/761133578758651914/H2_2.png?width=679&height=679"
         },
         fields: [
             {
                 name: "Información",
-                value: "Por favor leer nuestras <#758542086113984522>."
+                value: "Por favor leer nuestras <#758542086113984522> para mantener un orden en el discord."
             },
             {
                 name: "Quieres pertenecer a Holland Cargo?",
-                value: "Por favor lee nuestros <#758759495743570030> y envia el formulario de ingreso y pronto un recruiter se contactará contigo."
+                value: "Por favor lee nuestros <#758759495743570030> y envia el formulario de ingreso y pronto un Recruiter se contactará contigo."
             },
             {
                 name: `Deseas solicitar algún rol?`,
-                value: "Por favor ve a <#758542185007415387> y adquiere tu rol."
+                value: "Por favor ve a <#761121208745459752> y adquiere tu rol."
             }
         ],
         footer: {

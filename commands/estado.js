0b184fb2f6be99abd67ea4e3d655d0fb9b026c15
hollandcargovtc <71852352+hollandcargovtc@users.noreply.github.com>
2020-10-01 02:45:16 -0500

    Update estado.js

--- commands/estado.js
+++ commands/estado.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   message.delete().catch() 
-    message.channel.send("Bien chingón perro:v!");
+    message.channel.send("Server y bots operativos");
   }
   module.exports.help = {
       name: "estado"

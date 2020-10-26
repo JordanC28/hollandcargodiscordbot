4dd105ef7555df665e1b63c7a32a5fc4b5065b30
hollandcargovtc <71852352+hollandcargovtc@users.noreply.github.com>
2020-10-25 22:56:23 -0500

    Update invitacion.js

--- commands/invitacion.js
+++ commands/invitacion.js
@@ -4,8 +4,8 @@ module.exports.run = async (client, message, args) => {
   message.delete().catch() 
 
     const embed = new Discord.RichEmbed()
-      .setTitle("Invitación Holland Cargo")
-      .setColor('PURPLE')
+      .setTitle("Invitación a Holland Cargo")
+      .setColor('#084d6e')
       .setDescription("acá te dejamos la invitación a nuestro **DISCORD** [https://discord.gg/4VbkeME](https://discord.gg/4VbkeME) para que puedas invitar a tus amigos a nuestro servidor.")
       .setFooter("Holland Cargo", client.user.avatarURL)
       .setThumbnail('https://media.discordapp.net/attachments/758548412244951062/761076086385541130/H2_1_copia.png?width=943&height=475')

663d02e0e2d606e61c0ae9a395447b48a73971d6
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-08 00:11:09 -0500

    -

--- commands/admin.js
+++ commands/admin.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
   } else {
       return message.reply("Lo lamento no puedes usar el comando debido a que no eres parte del **STAFF** o estás en el canal equivocado").then(m => m.delete(5000))
   }
-      message.channel.send('Hola acá te dejamos unos comandos que puedes usar como administrador del servidor.**\n```\n'+
+      message.channel.send('Hola acá te dejamos unos comandos que puedes usar como administrador del servidor.\n```\n'+
                       '-> '+prefix+'report :: reporta a un usuarion.\n'+
                       '-> '+prefix+'say    :: Dice algo con el bot.\n'+
                       '-> '+prefix+'clear + cantidad :: Limpia mensajes en el servidor\n'+

e2ac070702f32a567730c0d8004a12e8d84783eb
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-12 10:45:10 -0500

    Create padrinos.js

---
+++ commands/padrinos.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  message.delete().catch() 
+
+    const embed = new Discord.RichEmbed()
+      .setTitle("Invitación a Latinos Gaming")
+      .setColor('#084d6e')
+      .setDescription("Acá te dejamos la invitación a nuestra **Comunidad de Discord** [https://discord.io/latinosgaming](https://discord.io/latinosgaming) para que puedas invitar a tus amigos a nuestro servidor.")
+      .setFooter("Latinos Gaming", client.user.avatarURL)
+      .setThumbnail('https://cdn.discordapp.com/attachments/762121963950702602/851154822680936483/image0.gif')
+      .setTimestamp()
+      .setURL("https://discord.io/latinosgaming")
+      
+      message.channel.send ({embed});
+        
+    }
+      module.exports.help = {
+        name: "discord",
+        category: "advertising",
+    
+    }

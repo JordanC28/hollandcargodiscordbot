d74fc8c85d6b4a89fd35dcf0894ac1881a1a38a2
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-12 11:17:20 -0500

    Update padrinos.js

--- commands/padrinos.js
+++ commands/padrinos.js
@@ -9,6 +9,7 @@ module.exports.run = async (client, message, args) => {
       .setDescription("Acá te dejamos la invitación a nuestra **Comunidad de Discord** [https://discord.io/latinosgaming](https://discord.io/latinosgaming) para que puedas invitar a tus amigos a nuestro servidor.")
       .setFooter("Latinos Gaming", client.user.avatarURL)
       .setImage('https://cdn.discordapp.com/attachments/762121963950702602/851154822680936483/image0.gif')
+      .setThumbnail('https://cdn.discordapp.com/attachments/762121963950702602/851154822680936483/image0.gif')
       .setTimestamp()
       .setURL("https://discord.io/latinosgaming")
       

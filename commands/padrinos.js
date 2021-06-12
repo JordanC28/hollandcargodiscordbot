2ce03103bde5143c2d45b3632ed0d8f6288defef
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-12 10:46:08 -0500

    Update padrinos.js

--- commands/padrinos.js
+++ commands/padrinos.js
@@ -16,7 +16,7 @@ module.exports.run = async (client, message, args) => {
         
     }
       module.exports.help = {
-        name: "discord",
+        name: "padrinos",
         category: "advertising",
     
     }

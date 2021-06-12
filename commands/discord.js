b0b0dbd201893d06cca24bead8a3152dcde5f643
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-12 12:16:54 -0500

    Update discord.js

--- commands/discord.js
+++ commands/discord.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
 
     const embed = new Discord.RichEmbed()
       .setTitle("Invitación a Latinos Gaming")
-      .setColor('#084d6e')
+      .setColor('#ff1616')
       .setDescription("Acá te dejamos la invitación a nuestra **Comunidad de Discord** [https://discord.io/latinosgaming](https://discord.io/latinosgaming) para que puedas invitar a tus amigos a nuestro servidor.")
       .setFooter("Latinos Gaming", client.user.avatarURL)
       .setThumbnail('https://cdn.discordapp.com/attachments/762121963950702602/851154822680936483/image0.gif')

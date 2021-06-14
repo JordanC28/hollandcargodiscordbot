a6bbe180a9647cbd91a016598e93f07f90088778
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-14 01:22:59 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,20 +1,50 @@
 const Discord = require("discord.js");
 
-if(command === 'server'){
+module.exports.run = async (client, message, args) => {
+    message.delete().catch() 
 
-    var server = message.guild;
-  
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+    let region = {
+        "brazil": ":flag_br: Brazil",
+        "europe": ":flag_eu: Central Europe",
+        "singapore": ":flag_sg: Singapore",
+        "us-central": ":flag_us: U.S. Central",
+        "sydney": ":flag_au: Sydney",
+        "us-east": ":flag_us: U.S. East",
+        "us-south": ":flag_us: U.S. South",
+        "us-west": ":flag_us: U.S. West",
+        "eu-west": ":flag_eu: Western Europe",
+        "vip-us-east": ":flag_us: VIP U.S. East",
+        "london": ":flag_gb: London",
+        "amsterdam": ":flag_nl: Amsterdam",
+        "hongkong": ":flag_hk: Hong Kong",
+        "russia": ":flag_ru: Russia",
+        "southafrica": ":flag_za:  South Africa"
+    };
     const embed = new Discord.RichEmbed()
-    .setThumbnail(server.iconURL)
-    .setAuthor(server.name, server.iconURL)
-    .addField('ID', server.id, true)
-    .addField('Region', server.region, true)
-    .addField('Creado el', server.joinedAt.toDateString(), true)
-    .addField('Dueño del Servidor', server.owner.user.username+'#'+server.owner.user.discriminator+' ('+server.owner.user.id +')', true)
-    .addField('Miembros', server.memberCount, true)
-    .addField('Roles', server.roles.size, true)
-    .setColor(0x66b3ff)
-    
-   message.channel.send({ embed });
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setColor('#ff1616')
+        .addField("Nombre: ", message.guild.name, true)
+        .addField("ID: ", message.guild.id, true)
+        .addField("Propietario: ", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+        .addField("Región: ", region[message.guild.region], true)
+        .addField("Total | Humanos | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
+        .addField("nivel de verificación: ", verifLevels[message.guild.verificationLevel], true)
+        .addField("Canales: ", message.guild.channels.size, true)
+        .addField("Roles: ", message.guild.roles.size, true)
+        .addField("Fecha de creación: ", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+        .setThumbnail(message.guild.SiconURL)
+    message.channel.send({embed});
+    message.delete().catch();
+}
 
-  }
+module.exports.help = {
+    name: "server",
+    description: "Latinos Gaming server"
+}

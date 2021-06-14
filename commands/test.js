76abf449dca39e9e93efd9921f18d4a343212a0c
Jordan <66105915+JordanC28@users.noreply.github.com>
2021-06-14 01:29:18 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -13,4 +13,14 @@ module.exports.help = {
     name: "test",
     aliases: []
 }
-    
+
+                     
+let ping = Math.floor(message.client.ping);
+message.channel.send("Cargando...").then(m => {
+  m.edit({embed: {
+    title: "Pong!:ping_pong: ",
+    description: `Mensaje: **${Math.floor(
+      m.createdTimestamp - Date.now()
+    )}ms**, API: **${ping}ms**`
+    }});
+});
